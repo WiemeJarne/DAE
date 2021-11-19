53910ab249728784c189eb33007927c76570113a
ozikov <jarne.wieme@student.howest.be>
2021-11-19 20:24:01 +0100

    19/11 oefening 4.2 week 7 afgewerkt

--- jaar1/C++/week6/labo/1DAE23_06_Wieme__Jarne/DrawFunctions/Game.h
+++ jaar1/C++/week6/labo/1DAE23_06_Wieme__Jarne/DrawFunctions/Game.h
@@ -15,16 +15,6 @@ float g_WindowHeight{ 500 };
 // Declare your own global variables here
 
 // Declare your own functions here
-void DrawSquares(const int numberOfSquares, float pointX, float pointY, int sideLenght);
-void DrawSquares(int numberOfSquares, Point2f point2, int sideLenght);
-
-void DrawEquilateralTriangle(const float bottomLeftPointX, const float bottomLeftPointY, const float sideLenght, const bool filled, const Color4f color);
-void DrawEquilateralTriangle(const Point2f bottomLeftPoint, const float sideLenght, const bool filled, const Color4f color);
-
-void DrawPentagram(const float middlePointX, const float middlePointY, const float radius, const float lineWidth, const Color4f color);
-void DrawPentagram(const Point2f middlePoint, const float radius, const float lineWidth, const Color4f color);
-
-void DrawLinearGradient(const Rectf rectangle, Color4f startColor, Color4f endColor);
 #pragma endregion ownDeclarations
 
 #pragma region gameFunctions											
