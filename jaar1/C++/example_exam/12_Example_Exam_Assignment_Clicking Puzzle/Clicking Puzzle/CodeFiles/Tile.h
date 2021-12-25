a9960f3070e97da9a2438f8b412df801b3402e58
ozikov <jarne.wieme@student.howest.be>
2021-12-25 16:03:33 +0100

    2d end assignment all assets finished

---
+++ jaar1/C++/example_exam/12_Example_Exam_Assignment_Clicking Puzzle/Clicking Puzzle/CodeFiles/Tile.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Tile 
+{
+public:
+	//Tile(const Rectf& dstRect, const std::string& textPath, int nrAnimals );
+
+	//void Draw();
+	//void CheckActivation(const Point2f& pos);
+	//bool CheckHit(const Point2f& pos);
+	//int GetCurrentAnimal( );
+	//void Deactivate();
+	//void Randomize( );
+
+private: 
+};
