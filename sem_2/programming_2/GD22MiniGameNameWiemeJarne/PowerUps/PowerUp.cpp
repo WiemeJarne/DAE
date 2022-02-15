9a0b171019c8269a417b998abe92d2e5b69307f2
ozikov <jarne.wieme@student.howest.be>
2022-02-15 15:38:58 +0100

    amp2 labo1

--- sem_2/programming_2/GD22MiniGameNameWiemeJarne/PowerUps/PowerUp.cpp
+++ sem_2/programming_2/GD22MiniGameNameWiemeJarne/PowerUps/PowerUp.cpp
@@ -1,16 +1,17 @@
 #include "pch.h"
 #include "PowerUp.h"
 #include "Texture.h"
+#include <iostream>
 
 PowerUp::PowerUp(const Point2f& center, PowerUp::Type type)
-	:m_Shape{center, 25.f}
+	:m_Shape{center, 30.f}
 	,m_Type{type}
 	,m_RotSpeed{360.f}
 	,m_Angle{}
 	,m_pTexture{ new Texture{"resources/Images/PowerUp.png"} }
 {
 	m_TextClip.left = 0.f;
-	m_TextClip.width = m_pTexture->GetWidth()/ m_pTexture->GetWidth();
+	m_TextClip.width = m_pTexture->GetWidth();
 	m_TextClip.height = m_pTexture->GetHeight() / 2;
 
 	if (m_Type == Type::brown)
@@ -36,8 +37,8 @@ void PowerUp::Update(float elapsedSec)
 void PowerUp::Draw() const
 {
 	Rectf destRect{};
-	destRect.left = m_Shape.center.y - m_Shape.radius;
-	destRect.bottom = m_Shape.center.x - m_Shape.radius;
+	destRect.left = m_Shape.center.x - m_Shape.radius;
+	destRect.bottom = m_Shape.center.y - m_Shape.radius;
 	destRect.width = m_Shape.radius * 2;
 	destRect.height = m_Shape.radius * 2;
 
@@ -46,5 +47,12 @@ void PowerUp::Draw() const
 
 bool PowerUp::IsOverlapping(const Rectf& rect) const
 {
+	if (sqrtf( powf(m_Shape.center.x - rect.left,2) + powf(m_Shape.center.y - rect.bottom,2) ) <= m_Shape.radius
+		&& sqrtf( powf(m_Shape.center.x - (rect.left + rect.width), 2) + powf(m_Shape.center.y - rect.bottom, 2) ) <= m_Shape.radius
+		&& sqrtf( powf(m_Shape.center.x - rect.left, 2) + powf(m_Shape.center.y - (rect.bottom + rect.height), 2) ) <= m_Shape.radius
+		&& sqrt ( powf(m_Shape.center.x - (rect.left + rect.width), 2) + powf(m_Shape.center.y - (rect.bottom + rect.height), 2) ) <= m_Shape.radius )
+	{
+		return true;
+	}
 	return false;
 }
