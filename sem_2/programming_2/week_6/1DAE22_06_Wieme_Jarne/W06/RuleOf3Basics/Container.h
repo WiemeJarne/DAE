2b654355739178bf8f3a7eb5606a7318901cb535
ozikov <jarne.wieme@student.howest.be>
2022-03-21 10:32:24 +0100

    week 6 prog2 laboles1

---
+++ sem_2/programming_2/week_6/1DAE22_06_Wieme_Jarne/W06/RuleOf3Basics/Container.h
@@ -0,0 +1,30 @@
+#pragma once
+
+class Container final
+{
+public:
+	explicit Container( int capacity  = 10); 
+	Container(const Container& container);
+	~Container( );
+
+	Container& operator=(const Container& rhs);
+
+	int Size( ) const;
+	int Capacity( ) const;
+	int Get( int index ) const;
+	void Set( int index, int value );
+	void PushBack( int element );
+
+	//int operator[](int index) const; // first version (only get)
+	int& operator[](int index) ; // second version (get and set)
+	int operator[](int index) const; // necessary to have a non modifying []
+	// If the value type is known to be a built-in type, 
+	// the const variant should return by value.
+
+private:
+	int m_Size;
+	int m_Capacity;
+	int *m_pElement;
+
+	void Reserve( int newCapacity );
+};
