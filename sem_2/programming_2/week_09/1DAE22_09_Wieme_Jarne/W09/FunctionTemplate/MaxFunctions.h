8f00f1ddc170b985ad95bc6746ff42b7607c162d
ozikov <jarne.wieme@student.howest.be>
2022-05-02 10:33:22 +0200

    prog2 lables 1 week 9

---
+++ sem_2/programming_2/week_09/1DAE22_09_Wieme_Jarne/W09/FunctionTemplate/MaxFunctions.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+#include "Fraction.h"
+
+template <typename T>
+T Max(T a, T b)
+{
+	return a < b ? b : a;
+}
+
+//int Max( int a, int b );
+//float Max( float a, float b );
+//const std::string& Max( const std::string& a, const std::string& b );
+//const Fraction& Max( const Fraction& a, const Fraction& b );
