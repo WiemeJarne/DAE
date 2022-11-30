0c769a4142371f3bd01d8af060343632cd4a830b
Jarne Wieme <jarne.wieme@telenet.be>
2022-11-30 21:55:34 +0100

    game mechanics unreal videos t.e.m. 27 gezien en gemaakt

---
+++ sem_3/Game_Mechanics/UE4Demo/Intermediate/Build/Win64/UE4/Inc/UE4Demo/UE4DemoGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef UE4DEMO_UE4DemoGameModeBase_generated_h
+#error "UE4DemoGameModeBase.generated.h already included, missing '#pragma once' in UE4DemoGameModeBase.h"
+#endif
+#define UE4DEMO_UE4DemoGameModeBase_generated_h
+
+#define UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_SPARSE_DATA
+#define UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_RPC_WRAPPERS
+#define UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAUE4DemoGameModeBase(); \
+	friend struct Z_Construct_UClass_AUE4DemoGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AUE4DemoGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/UE4Demo"), NO_API) \
+	DECLARE_SERIALIZER(AUE4DemoGameModeBase)
+
+
+#define UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAUE4DemoGameModeBase(); \
+	friend struct Z_Construct_UClass_AUE4DemoGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AUE4DemoGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/UE4Demo"), NO_API) \
+	DECLARE_SERIALIZER(AUE4DemoGameModeBase)
+
+
+#define UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AUE4DemoGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AUE4DemoGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AUE4DemoGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AUE4DemoGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AUE4DemoGameModeBase(AUE4DemoGameModeBase&&); \
+	NO_API AUE4DemoGameModeBase(const AUE4DemoGameModeBase&); \
+public:
+
+
+#define UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AUE4DemoGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AUE4DemoGameModeBase(AUE4DemoGameModeBase&&); \
+	NO_API AUE4DemoGameModeBase(const AUE4DemoGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AUE4DemoGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AUE4DemoGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AUE4DemoGameModeBase)
+
+
+#define UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_12_PROLOG
+#define UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_SPARSE_DATA \
+	UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_RPC_WRAPPERS \
+	UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_INCLASS \
+	UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_SPARSE_DATA \
+	UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> UE4DEMO_API UClass* StaticClass<class AUE4DemoGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID UE4Demo_Source_UE4Demo_UE4DemoGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ sem_3/Game_Mechanics/UE4Demo/Intermediate/Build/Win64/UE4Demo/Shipping/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30145
+#include "C:/Users/jarne/Documents/DAE/sem_3/Game_Mechanics/UE4Demo/Intermediate/Build/Win64/UE4Demo/Shipping/Engine/SharedPCH.Engine.ShadowErrors.h"
