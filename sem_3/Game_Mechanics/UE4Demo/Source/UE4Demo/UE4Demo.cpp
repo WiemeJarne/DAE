8407d27ab948d5a1ec139d0c4895cd7bb91250a8
ozikov <jarne.wieme@student.howest.be>
2022-12-05 16:42:24 +0100

    game mechanics unreal project momenteel op google drive gezet en beetje progres voor unreal game

--- sem_3/Game_Mechanics/UE4Demo/Source/UE4Demo/UE4Demo.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UE4Demo.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4Demo, "UE4Demo" );
