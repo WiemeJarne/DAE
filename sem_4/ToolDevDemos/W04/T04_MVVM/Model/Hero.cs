1c0af87d099a605b96f1f5062641145935d41320
WiemeJarne <jarne.wieme@telenet.be>
2023-03-13 10:33:11 +0100

    tooldev w04 demo

---
+++ sem_4/ToolDevDemos/W04/T04_MVVM/Model/Hero.cs
@@ -0,0 +1,57 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace T04_MVVM.Model
+{
+    public class Hero : ObservableObject
+    {
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private string _realName;
+
+        public string RealName
+        {
+            get { return _realName; }
+            set
+            {
+                _realName = value;
+                OnPropertyChanged(nameof(RealName));
+            }
+        }
+
+        private string _description;
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+
+        public BitmapImage Image { get { return new BitmapImage(new Uri("pack://application:,,,/Resources/superhero.jpg", UriKind.Absolute)); } }
+
+        public override string ToString()
+        {
+            return $"Hero details: \n- Name: {Name}\n - Real name: {RealName}\n - Description: {Description}";
+        }
+    }
+}
---
+++ sem_4/ToolDevDemos/W04/T04_MVVM/ViewModel/HeroPageVM.cs
@@ -0,0 +1,69 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T04_MVVM.Model;
+
+namespace T04_MVVM.ViewModel
+{
+    internal class HeroPageVM : ObservableObject
+    {
+        private string userName = "ozikov";
+
+        public string UserName
+        {
+            get { return userName; }
+            set
+            { 
+                userName = value; 
+                OnPropertyChanged(nameof(UserName));
+            }
+        }
+        public Hero CurrentHero { get; set; }
+        = new Hero()
+        {
+            Name = "",
+            RealName = "Real name of me",
+            Description = "this is the discription"
+        };
+
+        public RelayCommand ResetCommand { get; private set; }
+        public RelayCommand SaveCommand { get; private set; }
+
+        public HeroPageVM()
+        {
+            ResetCommand = new RelayCommand(ResetHero);
+            SaveCommand = new RelayCommand(SaveHero, CanSaveHero);
+
+            CurrentHero.PropertyChanged += CurrentHero_PropertyChanged;
+        }
+
+        private void CurrentHero_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            SaveCommand.NotifyCanExecuteChanged();
+        }
+
+        void ResetHero()
+        {
+            UserName = "no user";
+            CurrentHero.Name = "(no name)";
+            CurrentHero.RealName = "(no real name)";
+            CurrentHero.Description = "(this ain't no hero)";
+        }
+
+        void SaveHero()
+        {
+            Console.WriteLine("User has saved the hero");
+            Console.WriteLine(CurrentHero);
+        }
+
+        bool CanSaveHero()
+        {
+            return !string.IsNullOrWhiteSpace(CurrentHero.Name)
+                && !string.IsNullOrWhiteSpace(CurrentHero.RealName);
+        }
+    }
+}
