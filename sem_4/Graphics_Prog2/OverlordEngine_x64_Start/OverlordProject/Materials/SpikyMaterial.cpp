226f65c7a8542cd7f1654b89dff55fde49493a21
Jarne Wieme <jarne.wieme@telenet.be>
2023-03-18 21:16:20 +0100

    gp2 spiky shader gemaakt

---
+++ sem_4/Graphics_Prog2/OverlordEngine_x64_Start/OverlordProject/Materials/SpikyMaterial.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "SpikyMaterial.h"
+
+SpikyMaterial::SpikyMaterial()
+	:Material<SpikyMaterial>(L"Effects/SpikyShader.fx")
+{
+
+}
+
+void SpikyMaterial::InitializeEffectVariables()
+{
+
+}
---
+++ sem_4/Graphics_Prog2/OverlordEngine_x64_Start/OverlordProject/Scenes/Week 4/SpikyScene.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Materials/SpikyMaterial.h"
+
+class SpikyScene final : public GameScene
+{
+public:
+	SpikyScene();
+	~SpikyScene() override = default;
+
+	SpikyScene(const SpikyScene& other) = delete;
+	SpikyScene(SpikyScene&& other) noexcept = delete;
+	SpikyScene& operator=(const SpikyScene& other) = delete;
+	SpikyScene& operator=(SpikyScene&& other) noexcept = delete;
+
+protected:
+	void Initialize() override;
+	void Update() override;
+	void Draw() override;
+	void OnGUI() override;
+
+private:
+	GameObject* m_pOctaSphere{};
+	SpikyMaterial* m_pSpikyMaterial{};
+	float m_CurrentYRotation{};
+};
