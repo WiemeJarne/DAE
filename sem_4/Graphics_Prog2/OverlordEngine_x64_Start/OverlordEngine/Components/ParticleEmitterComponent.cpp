fdb8044daa844cd49ee67c202c95ef5657d751f1
WiemeJarne <94701784+WiemeJarne@users.noreply.github.com>
2023-05-08 20:28:15 +0200

    finnished implementing particle system

--- sem_4/Graphics_Prog2/OverlordEngine_x64_Start/OverlordEngine/Components/ParticleEmitterComponent.cpp
+++ sem_4/Graphics_Prog2/OverlordEngine_x64_Start/OverlordEngine/Components/ParticleEmitterComponent.cpp
@@ -16,7 +16,7 @@ ParticleEmitterComponent::ParticleEmitterComponent(const std::wstring& assetFile
 
 ParticleEmitterComponent::~ParticleEmitterComponent()
 {
-	delete m_ParticlesArray;
+	delete[] m_ParticlesArray;
 	m_pVertexBuffer->Release();
 }
 
@@ -29,7 +29,7 @@ void ParticleEmitterComponent::Initialize(const SceneContext& sceneContext)
 
 	CreateVertexBuffer(sceneContext);
 
-	m_pParticleTexture = ContentManager::Load<TextureData>(L"Resources/Textures/Smoke.png");
+	m_pParticleTexture = ContentManager::Load<TextureData>(m_AssetFile);
 }
 
 void ParticleEmitterComponent::CreateVertexBuffer(const SceneContext& sceneContext)
@@ -48,7 +48,7 @@ void ParticleEmitterComponent::CreateVertexBuffer(const SceneContext& sceneConte
 
 void ParticleEmitterComponent::Update(const SceneContext& sceneContext)
 {
-	float particleInterval{ ((m_EmitterSettings.maxEnergy + m_EmitterSettings.minEnergy) / 2.f) / m_ParticleCount };
+	float particleInterval{ ((m_EmitterSettings.minEnergy + m_EmitterSettings.maxEnergy) / 2.f) / m_ParticleCount };
 
 	const float elapsedSec{ sceneContext.pGameTime->GetElapsed() };
 
@@ -56,28 +56,28 @@ void ParticleEmitterComponent::Update(const SceneContext& sceneContext)
 
 	m_ActiveParticles = 0u;
 
-	D3D11_MAPPED_SUBRESOURCE* pData{};
-
-	HANDLE_ERROR(sceneContext.d3dContext.pDeviceContext->Map(m_pVertexBuffer, 0u, D3D11_MAP_WRITE_DISCARD, 0u, pData));
+	D3D11_MAPPED_SUBRESOURCE subResource{};
+	sceneContext.d3dContext.pDeviceContext->Map(m_pVertexBuffer, 0u, D3D11_MAP_WRITE_DISCARD, 0u, &subResource);
 	
 	VertexParticle* pBuffer{};
-	pBuffer = reinterpret_cast<VertexParticle*>(pData);
+	pBuffer = reinterpret_cast<VertexParticle*>(subResource.pData);
 
 	for (UINT index{}; index < m_ParticleCount; ++index)
 	{
-		auto pParticle{ m_ParticlesArray + index };
+		Particle& pParticle{ m_ParticlesArray[index] };
 
-		if (pParticle->isActive)
-			UpdateParticle(*pParticle, elapsedSec);
+		if (pParticle.isActive)
+			UpdateParticle(pParticle, elapsedSec);
 		
-		if (!pParticle->isActive && m_LastParticleSpawn > particleInterval)
+		if (!pParticle.isActive && m_LastParticleSpawn >= particleInterval)
 		{
-			SpawnParticle(*pParticle);
+			SpawnParticle(pParticle);
+			m_LastParticleSpawn -= particleInterval;
 		}
 
-		if (pParticle->isActive)
+		if (pParticle.isActive)
 		{
-			pBuffer[m_ActiveParticles] = pParticle->vertexInfo;
+			pBuffer[m_ActiveParticles] = pParticle.vertexInfo;
 			++m_ActiveParticles;
 		}
 	}
@@ -92,25 +92,80 @@ void ParticleEmitterComponent::UpdateParticle(Particle& p, float elapsedTime) co
 
 	p.currentEnergy -= elapsedTime;
 
+	if (p.currentEnergy <= 0.f)
+	{
+		p.isActive = false;
+		return;
+	}
+
 	auto pos{ XMLoadFloat3(&p.vertexInfo.Position) };
-	auto velocity{ XMLoadFloat3(&p.vertexInfo.Position) };
-	pos += elapsedTime * velocity;
+	auto velocity{ XMLoadFloat3(&m_EmitterSettings.velocity) };
+	pos += velocity * elapsedTime;
 	XMStoreFloat3(&p.vertexInfo.Position, pos);
 
 	p.vertexInfo.Color = m_EmitterSettings.color;
-	p.vertexInfo.Color.w = m_EmitterSettings.color.w * (p.currentEnergy / p.totalEnergy);
 
-	p.vertexInfo.Size = p.initialSize * p.sizeChange * (p.currentEnergy / p.totalEnergy);
+	const float lifePercentage{ p.currentEnergy / p.totalEnergy };
+	const float fadeDelay{ 2.f };
+	p.vertexInfo.Color.w = m_EmitterSettings.color.w * lifePercentage * fadeDelay;
+
+	p.vertexInfo.Size = p.initialSize * ( 1.f + (p.sizeChange - 1.f) * (1.f - lifePercentage));
 }
 
 void ParticleEmitterComponent::SpawnParticle(Particle& p)
 {
 	p.isActive = true;
+
+	p.totalEnergy = MathHelper::randF(m_EmitterSettings.minEnergy, m_EmitterSettings.maxEnergy);
+	p.currentEnergy = p.totalEnergy;
+
+	XMVECTOR randomDirection{ 1.f, 0.f, 0.f };
+	auto randomRotationMatrix{ DirectX::XMMatrixRotationRollPitchYaw(MathHelper::randF(-XM_PI, XM_PI), MathHelper::randF(-XM_PI, XM_PI), MathHelper::randF(-XM_PI, XM_PI)) };
+	XMVector2TransformNormal(randomDirection, randomRotationMatrix);
+
+	XMStoreFloat3(&p.vertexInfo.Position, XMLoadFloat3(&GetTransform()->GetWorldPosition()) + randomDirection * MathHelper::randF(m_EmitterSettings.minEmitterRadius, m_EmitterSettings.maxEmitterRadius));
+	
+	p.initialSize = MathHelper::randF(m_EmitterSettings.minSize, m_EmitterSettings.maxSize);
+	p.vertexInfo.Size = p.initialSize;
+
+	p.sizeChange = MathHelper::randF(m_EmitterSettings.minScale, m_EmitterSettings.maxScale);
+
+	p.vertexInfo.Rotation = MathHelper::randF(-XM_PI, XM_PI);
+
+	p.vertexInfo.Color = m_EmitterSettings.color;
 }
 
-void ParticleEmitterComponent::PostDraw(const SceneContext& /*sceneContext*/)
+void ParticleEmitterComponent::PostDraw(const SceneContext& sceneContext)
 {
-	TODO_W9(L"Implement PostDraw")
+	m_pParticleMaterial->SetVariable_Matrix(L"gWorldViewProj", sceneContext.pCamera->GetViewProjection());
+	m_pParticleMaterial->SetVariable_Matrix(L"gViewInverse", sceneContext.pCamera->GetViewInverse());
+	m_pParticleMaterial->SetVariable_Texture(L"gParticleTexture", m_pParticleTexture->GetShaderResourceView());
+
+	auto pDeviceContext{ sceneContext.d3dContext.pDeviceContext };
+	const auto& pTechniqueContext{ m_pParticleMaterial->GetTechniqueContext() };
+
+	//set the InputLayout
+	pDeviceContext->IASetInputLayout(pTechniqueContext.pInputLayout);
+
+	//set the PrimitiveTopology
+	pDeviceContext->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_POINTLIST);
+
+	//set the VertexBuffer
+	const UINT stride{ sizeof(VertexParticle) };
+	const UINT offset{};
+	pDeviceContext->IASetVertexBuffers(0u, 1u, &m_pVertexBuffer, &stride, &offset);
+
+	D3DX11_TECHNIQUE_DESC techniqueDesc;
+	pTechniqueContext.pTechnique->GetDesc(&techniqueDesc);
+	
+	for (uint32_t index{}; index < techniqueDesc.Passes; ++index)
+	{
+		//Apply the pass
+		pTechniqueContext.pTechnique->GetPassByIndex(0)->Apply(0, pDeviceContext);
+
+		//Draw the vertices
+		pDeviceContext->Draw(m_ActiveParticles, 0u);
+	}
 }
 
 void ParticleEmitterComponent::DrawImGui()
